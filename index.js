385768a3aa4aabe21dc66ba81c28a869a682a065
Julianne Huynh <JulianneHuynh@yahoo.com>
2022-11-04 16:01:36 -0500

    commit tree 8c061ea8376ee89f6e7dd3aa3210c327b94ca61c

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Add your code here
+// // Add your code here
+
+// const formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//     .then (function (object){
+//       console.log(object);
+//     })
+//  .catch(function (error) {
+//   alert("Bad things! Ragnarők!");
+//   console.log(error.message);
+// });
+  // configurationObject contains 3 core components
+  // - std POST, -request HTTP verb, -headers,-body
+
+function submitData (name, email){
+  const configurationObject = {
+    method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({name,email}),
+  };
+  return fetch ("http://localhost:3000/users", configurationObject)
+    .then (function (response){
+      return response.json();
+    })
+    .then (function (object){
+      document.body.innerHTML = object["id"]
+    })
+    .catch(function (error) {
+      alert("What went wrong");
+      document.body.innerHTML = error.message;
+      });
+    }
+
+    submitData("Julianne","abc@gmail.com"); 
+
+ 
